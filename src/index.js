4d00dea0332ed9f34e36be05ab6e544428f7868b
Kévin Casagrande <training.kcasagrande@xoxy.net>
2019-03-28 18:19:30 +0100

    Determine the winner.

--- src/index.js
+++ src/index.js
@@ -39,7 +39,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		const winner = determineWinner(this.state.squares);
+    let status;
+		if(winner) {
+			status = 'Winner : ' + winner;
+		}
+		else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		}
 
     return (
       <div>
@@ -78,6 +85,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function determineWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	for(let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+		const [a, b, c] = lines[lineIndex];
+		if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
 // ========================================
